fe7fc5af03e892b3a43fd4e6602560e2324cffe0
Surya Gaddipati <sgaddipati@groupon.com>
2017-08-30 18:50:04 -0500

    parse payload for push

--- src/main/scala/surya/onGithub/actors/github/GithubActor.scala
+++ src/main/scala/surya/onGithub/actors/github/GithubActor.scala
@@ -1,13 +1,14 @@
 package surya.onGithub.actors.github
 
-import akka.actor.Actor
+import akka.actor.{Actor, PoisonPill}
 import com.google.common.io.BaseEncoding
 import surya.onGithub.di.Services
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 class GithubActor(services: Services) extends Actor{
   override def receive: Receive = {
     case hookShot:HookShot => {
-//
       import net.caoticode.buhtig.Converters._
       val repo = hookShot.repo
       val org = hookShot.org
@@ -15,11 +16,17 @@ class GithubActor(services: Services) extends Actor{
       val encodedContents = (onGithubFile \\ "content").values.asInstanceOf[String]
       val image = new String(BaseEncoding.base64().decode(encodedContents.trim.replaceAll("\\n","")))
       sender()! image.trim.replaceAll("\\n","")
-
+      self ! PoisonPill
     }
   }
 }
 case  class HookShot(hookId: String, event:String,payload: String){
-  def repo:String = "pulihora"
-  def org:String = "suryagaddipati"
+  val payloadJson = parse(payload)
+  val repo =  event match {
+    case "push" =>  (payloadJson \ "repository" \ "name").values.asInstanceOf[String]
+  }
+  val org =  event match {
+    case "push" => (payloadJson \ "repository" \"owner" \ "name").values.asInstanceOf[String]
+  }
+
 }
